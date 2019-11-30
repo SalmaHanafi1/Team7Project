ccc83c76b3856f9f6c1e1d7091e4c723fff41403
rajaa lqadi <rlqadi@uncc.edu>
2019-11-30 03:05:20 +0000

    Generating an error if no images attached to new trail

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,3 +1,10 @@
 class Trail < ApplicationRecord
     has_many_attached :images
+    validate :Name, presence: true
+    validate :Location, presence: true
+    
+    
+def thumbnail input
+    return self.images[input].variant(resize: '300x300!').processed
+end
 end
