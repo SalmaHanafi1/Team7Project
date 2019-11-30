9960aa3164bc8685233dc43e07415d064467afbd
rajaa lqadi <rlqadi@uncc.edu>
2019-11-30 00:40:48 +0000

    Complete Create and show

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,8 +1,19 @@
 class TrailsController < ApplicationController
+    def show
+        @trail = Trail.find(params[:id])
+    end
     def new
     end
     
     def create
-        render plain: params[:trail].inspect
+        @trail = Trail.new(trail_params)
+        
+        @trail.save
+        redirect_to @trail
     end
 end
+
+private
+    def trail_params
+        params.require(:trail).permit(:Name, :Location, :Description)
+    end
