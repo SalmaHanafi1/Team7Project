537a9d88d53eec7420dbf484645923cf9dbc45cb
rajaa lqadi <rlqadi@uncc.edu>
2019-11-30 03:19:23 +0000

    validate image type

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,10 +1,23 @@
 class Trail < ApplicationRecord
     has_many_attached :images
-    validate :Name, presence: true
-    validate :Location, presence: true
+    validates :Name, presence: true
+    validates :Location, presence: true
+    validate :image_type
     
     
-def thumbnail input
+    def thumbnail input
     return self.images[input].variant(resize: '300x300!').processed
-end
+    end
+
+    private 
+    def image_type
+        if images.attached? == false
+            errors.add(:images, "are missing!")
+        end
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png'))
+               errors.add(:images, 'needs to be a JPEG or PNG') 
+            end
+        end
+    end
 end
