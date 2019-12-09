efc9b31e027fc4123f5a3bc0b62a4f95512da547
”rajaa <rlqadi@uncc.edu>
2019-12-09 17:13:51 +0000

    Add feedbacks routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'mainpage/index'
   
-  resources :trails
+  resources :trails do
+    resources :feed_backs
+  end
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
