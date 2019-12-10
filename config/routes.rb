2509ed5b8b640ede292732497c3592b6d30ccf72
”rajaa <rlqadi@uncc.edu>
2019-12-10 02:18:38 +0000

    Add review routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
   
   
  
-  resources :trails
+  resources :trails do
+    resources :reviews
+  end
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
