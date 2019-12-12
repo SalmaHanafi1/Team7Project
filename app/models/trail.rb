5932140dc4eb9aaeadffe8e9639ede2d1bdd5330
rajaa lqadi <rlqadi@uncc.edu>
2019-12-12 06:06:12 +0000

    add dependency of reviews on trails upon deletion

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,6 +1,7 @@
 class Trail < ApplicationRecord
     has_many_attached :images
-    has_many :reviews
+    has_many :reviews, :dependent => :destroy
+    
     validates :Name, presence: true, length: { minimum: 5}
     validates :Location, presence: true, length: { minimum: 15}
     validates :Description, presence: true, length: { minimum: 20}
